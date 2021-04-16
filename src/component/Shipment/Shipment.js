ff44a46bcca88df65409749a56dd9b35428193b8
shahidul jahid <shahiduljahid3@gmail.com>
2021-04-17 04:29:50 +0800

    sj

--- src/component/Shipment/Shipment.js
+++ src/component/Shipment/Shipment.js
@@ -3,7 +3,7 @@ import React from "react";
 const Shipment = () => {
   return (
     <div >
-      <div className="mt-5 container d-flex justify-content-around col-xl-6"><h1>Thanks For Shopping</h1></div>
+      <div className="mt-5 text-center container d-flex justify-content-around col-xl-6"><h1>Thanks For Shopping</h1></div>
         <div className="container col-xl-6 d-flex justify-content-around">
         
         <img className="img-fluid" src="https://media.giphy.com/media/6A5aeKoUiIH7DwbR3R/giphy.gif" alt=""/>
